25fc0db9ef70ce93dd97120c734f32a601f683f0
vishu vatsay <vishusharma9520@gmail.com>
2025-12-29 20:24:46 +0530

    first phase of mini project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Material UI Demo</h1>
+      <Button>Click me!</Button>
+    </>
+  )
+}
+
+export default App
