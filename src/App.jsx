31b51d9765a294eb251367da9797be6da2b8519f
vishu vatsay <vishusharma9520@gmail.com>
2025-12-30 10:06:28 +0530

    delete button addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 import './App.css'
 
 function App() {
  
-
+let handleClick=()=>{
+  console.log("button was clicked!");
+}
   return (
     <>
       
       <h1>Material UI Demo</h1>
-      <Button>Click me!</Button>
+      <Button variant="contained" onClick={handleClick} startIcon={<DeleteIcon/>}>
+        Delete!
+        </Button>
+        &nbsp;
+        <Button variant="contained" onClick={handleClick} color='success' size='large'>
+        Click me2!
+        </Button>
     </>
   )
 }
